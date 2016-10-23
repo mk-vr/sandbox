e84dead8ed6e579efcfa83e7198242e91c59761f
Michael Knepprath <michael@mknepprath.com>
2016-10-23 10:54:24 -0500

    some minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'aframe';
+import 'aframe-animation-component';
+import 'aframe-text-component';
+import 'babel-polyfill';
+import {Entity, Scene} from 'aframe-react';
+import React from 'react';
 
-class App extends Component {
-  render() {
+import Camera from './components/Camera';
+import Sky from './components/Sky';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {color: 'red'};
+  }
+
+  changeColor() {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)]
+    });
+  }
+
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Scene>
+        <Camera>
+          <a-cursor
+            animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
+>
+          </a-cursor>
+        </Camera>
+
+        <Sky src="url(https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg)"/>
+
+        <Entity light={{type: 'ambient', color: '#888'}}/>
+        <Entity light={{type: 'directional', intensity: 0.5}} position='-1 1 0'/>
+        <Entity light={{type: 'directional', intensity: 1}} position='1 1 0'/>
+
+        <Entity
+          animation__rot={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
+          animation__sca={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+          geometry='primitive: box'
+          material={{color: this.state.color, opacity: 0.6}}
+          position='0 -0.5 -3'
+          onClick={this.changeColor.bind(this)}>
+          <Entity
+            animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+            geometry='primitive: box; depth: 0.2; height: 0.2; width: 0.2'
+            material={{color: '#24CAFF'}}/>
+        </Entity>
+      </Scene>
     );
   }
 }
 
-export default App;
+export default App
